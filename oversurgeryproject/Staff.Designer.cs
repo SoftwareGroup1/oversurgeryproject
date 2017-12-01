972f8e1a323ff1017328b46994df10c5ff58c303
Kishan Dayasena <kishandayasena@gmail.com>
2017-12-01 00:02:40 +0000

    design changes

--- oversurgeryproject/Staff.Designer.cs
+++ oversurgeryproject/Staff.Designer.cs
@@ -60,9 +60,9 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(886, 75);
+            this.pictureBox1.Location = new System.Drawing.Point(838, 61);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(207, 50);
+            this.pictureBox1.Size = new System.Drawing.Size(255, 50);
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
             // 
